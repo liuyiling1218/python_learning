d4184c23eed80a9e102b4291f5637123966c330c
liuyiling <ly1[0932313]>
2023-09-05 11:53:55 +0800

    feat:python 练习

---
+++ day_01/Demo01.py
@@ -0,0 +1,21 @@
+def collatz(number: int):
+    if number % 2 == 0:
+        result = number // 2
+    else:
+        result = 3 * number + 1
+    print(result)
+    return result
+
+
+def main():
+    try:
+        number = int(input("Enter number:"))
+        while True:
+            if number != 1:
+                number = collatz(number)
+    except ValueError:
+        print("输入错误，请输入一个整数。")
+
+
+if __name__ == '__main__':
+    main()
