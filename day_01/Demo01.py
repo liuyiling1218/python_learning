5eaff57cd0f5be6983ff06177f9dd0a0cfa1c33c
liuyiling <ly1[0932313]>
2023-09-05 13:52:51 +0800

    feat:python 练习

--- day_01/Demo01.py
+++ day_01/Demo01.py
@@ -1,10 +1,10 @@
 def collatz(number: int):
     if number % 2 == 0:
-        result = number // 2
+        integer = number // 2
     else:
-        result = 3 * number + 1
-    print(result)
-    return result
+        integer = 3 * number + 1
+    print(integer)
+    return integer
 
 
 def foo():
